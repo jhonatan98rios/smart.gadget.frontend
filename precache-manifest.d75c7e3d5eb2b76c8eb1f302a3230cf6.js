e58996aec80bc3c69671cd629e604f9935fc36a7
Jhonatan Maia Rios <jhonatan.rios@the9level.com>
2020-09-26 04:09:58 -0300

    Updates

---
+++ precache-manifest.d75c7e3d5eb2b76c8eb1f302a3230cf6.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd96afd9bb180a39f1574023884bd561",
+    "url": "/smart.gadget.frontend/index.html"
+  },
+  {
+    "revision": "f978c4f6edcb3d6d978d",
+    "url": "/smart.gadget.frontend/static/css/main.6c15c020.chunk.css"
+  },
+  {
+    "revision": "10766e8e3802700bc852",
+    "url": "/smart.gadget.frontend/static/js/2.60aaf350.chunk.js"
+  },
+  {
+    "revision": "7ed00624bc24626253de2c7d704e262d",
+    "url": "/smart.gadget.frontend/static/js/2.60aaf350.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f978c4f6edcb3d6d978d",
+    "url": "/smart.gadget.frontend/static/js/main.1134d400.chunk.js"
+  },
+  {
+    "revision": "072ce7d4b94d55a7df2d",
+    "url": "/smart.gadget.frontend/static/js/runtime-main.f0a106b1.js"
+  },
+  {
+    "revision": "49c0ed107af4748b4b3fde0cec76b3ed",
+    "url": "/smart.gadget.frontend/static/media/1889554a5177856184d679dcb180259d.49c0ed10.png"
+  },
+  {
+    "revision": "b8bb03465e38e893c3742196ec2396f1",
+    "url": "/smart.gadget.frontend/static/media/200222429a88b72befebf180eedf7b05.b8bb0346.png"
+  },
+  {
+    "revision": "733a5c1f33c0e24d1946191aab3ceb36",
+    "url": "/smart.gadget.frontend/static/media/20a6fe2c83b8b159feb59930313c4799.733a5c1f.png"
+  },
+  {
+    "revision": "49c0ed107af4748b4b3fde0cec76b3ed",
+    "url": "/smart.gadget.frontend/static/media/7fcd4792a7908e070701d8fb24a00d1f.49c0ed10.png"
+  },
+  {
+    "revision": "1fd7b617f9c6582d6d158f358560759a",
+    "url": "/smart.gadget.frontend/static/media/83e7b26d7c77d8d0de0bccb6696412b5.1fd7b617.png"
+  },
+  {
+    "revision": "86c55e13178f5a761acaa4456483a982",
+    "url": "/smart.gadget.frontend/static/media/Info.86c55e13.svg"
+  },
+  {
+    "revision": "997f02b19cebca4ba8b49acfb78df67a",
+    "url": "/smart.gadget.frontend/static/media/SGNP760XBEXW1_PRD_447_2.997f02b1.png"
+  },
+  {
+    "revision": "3660bf93a85f0753da159111d4f660b0",
+    "url": "/smart.gadget.frontend/static/media/bb137879e5796f5c6005641f09798882.3660bf93.png"
+  },
+  {
+    "revision": "8659515094d85059439811bbedcbbc39",
+    "url": "/smart.gadget.frontend/static/media/burguer.86595150.svg"
+  },
+  {
+    "revision": "a063073f64d7dfe629b460da3aa6795e",
+    "url": "/smart.gadget.frontend/static/media/ccd6e0e75af818db21d12f1cba264cb2.a063073f.png"
+  },
+  {
+    "revision": "8c32eee04b1b7d87176faca5c4cc47d3",
+    "url": "/smart.gadget.frontend/static/media/close.8c32eee0.svg"
+  },
+  {
+    "revision": "a6fe10f4adfd6dae2c50500ba52522e5",
+    "url": "/smart.gadget.frontend/static/media/front_4_22.a6fe10f4.png"
+  },
+  {
+    "revision": "a9879b859009cef843b9b4ff7c2a6b4c",
+    "url": "/smart.gadget.frontend/static/media/macbook-apple-pro-retina-intel-core-i5-8gb-ssd-128gb-macos-13-3-cinza-espacial-muhn2bz-a_1571255868_gg.a9879b85.png"
+  },
+  {
+    "revision": "7340dc6566195433d7eef7ace9c699f1",
+    "url": "/smart.gadget.frontend/static/media/notebook-acer-pentium-gold-4gb-500gb-tela-15-6-windows-10-aspire-3-a315-53-p884-55000029.7340dc65.png"
+  },
+  {
+    "revision": "e4e3e46865618ce91ca21e2caa19e754",
+    "url": "/smart.gadget.frontend/static/media/notebook-dell-core-i5-8265u-8gb-1tb-tela-hd-15-6-linux-inspiron-i15-3583-d3xp-50001437.e4e3e468.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/smart.gadget.frontend/precache-manifest.d75c7e3d5eb2b76c8eb1f302a3230cf6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/smart.gadget.frontend/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
